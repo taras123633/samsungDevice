af25c41b345f196d8ccc0b6360d263cc1f48676d
Taras Berezka <taras.berezka@globallogic.com>
2025-01-04 09:42:39 +0200

    Make changes

---
+++ bar.js
@@ -0,0 +1,13 @@
+
+function f(){
+
+<<<<<<< HEAD
+    const a=113;
+=======
+    const a=111;
+>>>>>>> dev
+
+    console.log("1baw=",a);
+
+}
+
---
+++ kok.js
@@ -0,0 +1,14 @@
+
+
+
+function f(){
+
+<<<<<<< HEAD
+    const a=54;
+    throw 11312;
+=======
+    const a=151;
+    throw 1;
+>>>>>>> dev
+
+}
